a598567664ec286bd01aa09457f2397f7bcc8e70
yourzinc <your_zinc@ewhain.net>
2023-01-17 15:32:26 +0900

    feat: initialize a project

---
+++ src/main/java/p/whatsinmycloset/WhatsinmyclosetApplication.java
@@ -0,0 +1,13 @@
+package p.whatsinmycloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsinmyclosetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsinmyclosetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/p/whatsinmycloset/WhatsinmyclosetApplicationTests.java
@@ -0,0 +1,13 @@
+package p.whatsinmycloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsinmyclosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
