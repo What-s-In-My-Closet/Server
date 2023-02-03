69dff22e72d77662060c391777a990929f0516d9
yourzinc <your_zinc@ewhain.net>
2023-02-03 15:25:18 +0900

    feat: 전체 회원 조회 테스트

--- src/test/java/p/whatsinmycloset/service/UserServiceTest.java
+++ src/test/java/p/whatsinmycloset/service/UserServiceTest.java
@@ -8,6 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 import p.whatsinmycloset.domain.User;
 import p.whatsinmycloset.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -33,7 +36,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void 중복_회원_예외() throws Exception {
+    public void 중복회원예외() throws Exception {
         //given
         User user1 = new User();
         user1.setEmail("thisemail@isnot.work");
@@ -50,5 +53,28 @@ class UserServiceTest {
         });
 
 //        fail("예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 전체회원조회() throws Exception {
+        //given
+
+        List<User> users = new ArrayList<>(3);
+
+        User user1 = new User();    user1.setEmail("thisemail1@isnot.work");
+        User user2 = new User();    user2.setEmail("thisemail2@isnot.work");
+        User user3 = new User();    user3.setEmail("thisemail3@isnot.work");
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+        //when
+        Long user1Id = userService.join(user1);
+        Long user2Id = userService.join(user2);
+        Long user3Id = userService.join(user3);
+
+        //then
+        Assertions.assertEquals(users, userService.findUsers());
     }
 }
